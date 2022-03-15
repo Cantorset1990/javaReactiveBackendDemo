7aa97803982e267748f8149e148bc0bc68b90412
Daniel Lim <48618069+Cantorset1990@users.noreply.github.com>
2022-03-15 22:04:35 +0800

    Update Configuration.java

--- src/main/java/com/projectroboticscontent/contentDatabaseAPI/Utility/Configuration.java
+++ src/main/java/com/projectroboticscontent/contentDatabaseAPI/Utility/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
 
     public static final String DeploymentWalletURL = "";
     public static final String DeploymentFrontEndURL = "";
-    public static final String blockchainIPAddress = "http://192.168.50.24:8080/api/v1";
+    public static final String blockchainIPAddress = "http://localhost:8080/api/v1";
 
 
 
